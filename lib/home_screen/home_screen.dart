b9e2e949d936a73da32186123e365587a2ba6d76
Mari0111 <nyalentine@gmail.com>
2022-04-26 10:09:31 +0800

    added background

--- lib/home_screen/home_screen.dart
+++ lib/home_screen/home_screen.dart
@@ -1,21 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:music_player/home_screen/home_screen_manager.dart';
+import 'package:music_player/music_player_screen/music_player_screen.dart';
+import 'package:music_player/service_locator.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+    getIt<HomeScreenManager>().updateSongs();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 3,
-        itemBuilder: (context, index) {
-          return const Card(
-            child: ListTile(
-              title: Text('Song Name'),
-            ),
-          );
-        },
-      ),
+      body: ValueListenableBuilder<List<String>>(
+          valueListenable: getIt<HomeScreenManager>().songListNotifier,
+          builder: (context, songList, child) {
+            return ListView.builder(
+              itemCount: songList.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: ListTile(
+                    title: Text(songList[index]),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const MusicPlayerScreen()),
+                      );
+                    },
+                  ),
+                );
+              },
+            );
+          }),
     );
   }
 }
---
+++ lib/music_player_screen/music_player_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MusicPlayerScreen extends StatelessWidget {
+  const MusicPlayerScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('Music Player')),
+        body: const Image(
+          image: AssetImage('assets/music1.jpg'),
+          fit: BoxFit.cover,
+          height: double.infinity,
+          width: double.infinity,
+          alignment: Alignment.center,
+        ));
+  }
+}
