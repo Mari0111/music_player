bb2d4e522485c31b614fb128d272c804743b9e38
Mari0111 <nyalentine@gmail.com>
2022-04-26 10:35:34 +0800

    progress bar added

--- lib/music_player_screen/music_player_screen.dart
+++ lib/music_player_screen/music_player_screen.dart
@@ -1,3 +1,4 @@
+import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
 import 'package:flutter/material.dart';
 
 class MusicPlayerScreen extends StatelessWidget {
@@ -7,12 +8,45 @@ class MusicPlayerScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: const Text('Music Player')),
-        body: const Image(
-          image: AssetImage('assets/music1.jpg'),
-          fit: BoxFit.cover,
-          height: double.infinity,
-          width: double.infinity,
-          alignment: Alignment.center,
+        body: Stack(
+          fit: StackFit.expand,
+          children: [
+            const Image(
+              image: AssetImage('assets/music1.jpg'),
+              fit: BoxFit.cover,
+              height: double.infinity,
+              width: double.infinity,
+              alignment: Alignment.center,
+            ),
+            ColoredBox(
+              color: Colors.white.withOpacity(0.3),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: EdgeInsets.all(40.0),
+                child: Column(
+                  children: [
+                    Spacer(),
+                    const ProgressBar(
+                      thumbColor: Colors.white,
+                      progressBarColor: Colors.white,
+                      baseBarColor: Color.fromARGB(255, 255, 159, 191),
+                      progress: Duration(minutes: 1),
+                      total: Duration(minutes: 3, seconds: 30),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.play_arrow,
+                        size: 30,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ));
   }
 }
